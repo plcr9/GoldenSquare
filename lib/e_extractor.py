bd7a112fe7b9c64221fa0f7f8d491aa06dcad208
plcr9 <paulcooperpc9@gmail.com>
2023-07-28 18:04:16 +0100

    E Extractor Refactor

--- lib/e_extractor.py
+++ lib/e_extractor.py
@@ -1,9 +1,11 @@
 def e_extractor(str):
     if "e" in str:
-        letter_es = [letter for letter in str if letter == "e"]
-        non_letter_es = [letter for letter in str if letter != "e"]
-        letters = letter_es + non_letter_es
-        return "".join(letters)
-
+        new_str = ""
+        for letter in str:
+            if letter == "e":
+                new_str = "e" + new_str
+            else:
+                new_str = new_str + letter
+        return new_str
     else:
         return str
