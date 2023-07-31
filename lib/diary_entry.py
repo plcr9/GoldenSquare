70fe413a535d3d9c85cd4e39ad5e208b7184b476
plcr9 <paulcooperpc9@gmail.com>
2023-07-31 18:47:51 +0100

    Diary entry and tests added

---
+++ lib/diary_entry.py
@@ -0,0 +1,31 @@
+import math
+
+class DiaryEntry():
+    def __init__(self, title, contents):
+        if title == "" or contents == "":
+            raise Exception("Diary entries must have a title and contents")
+        self._title = title
+        self._contents = contents
+        self._read_so_far = 0
+
+    def format(self):
+        return f"{self._title}: {self._contents}"
+    
+    def count_words(self):
+        words = self.format().split()
+        return len(words)
+
+    def reading_time(self, wpm):
+        if wpm == 0:
+            raise Exception("Cannot calculate reading time with wpm of 0")
+        contents_word_count = len(self._contents.split())
+        return math.ceil(contents_word_count / wpm)
+    
+    def reading_chunk(self, wpm, minutes):
+        words_user_can_read = wpm * minutes
+        words = self._contents.split()
+        chunk_start = self._read_so_far
+        chunk_end = self._read_so_far + words_user_can_read
+        chunk_words = words[chunk_start:chunk_end]
+        self._read_so_far = chunk_end
+        return " ".join(chunk_words)
---
+++ tests/test_diary_entry.py
@@ -0,0 +1,65 @@
+import pytest
+from lib.diary_entry import DiaryEntry
+
+'''Given an empty title, it raises an error'''
+def test_errors_on_empty_title():
+    with pytest.raises(Exception) as err:
+        DiaryEntry("", "my contents")
+    assert str(err.value) == "Diary entries must have a title and contents"
+
+'''Given an empty contents, it raises an error'''
+def test_errors_on_empty_contents():
+    with pytest.raises(Exception) as err:
+        DiaryEntry("my title", "")
+    assert str(err.value) == "Diary entries must have a title and contents"
+
+'''Given a title and contents, format returns formatted entry, like 'My Title: These are the contents'''
+def test_formats_with_title_and_contents():
+    diary_entry = DiaryEntry("My Title", "Some contents")
+    result = diary_entry.format()
+    assert result == "My Title: Some contents"
+
+'''Given a title and some contents, count_words returns the number of words in the title and contents'''
+def test_counts_words_in_both_title_and_contents():
+    diary_entry = DiaryEntry("My Title", "Some contents")
+    result = diary_entry.count_words()
+    assert result == 4
+
+'''Given a wpm of 2, reading_time returns the number of minutes it will take to read the contents'''
+def test_reading_time_with_two_wpm_and_two_words():
+    diary_entry = DiaryEntry("My Title", "Some contents")
+    result = diary_entry.reading_time(2)
+    assert result == 1
+
+'''Given a wpm of 2 and text of 4 words, reading_time returns 2 minutes'''
+def test_reading_time_with_two_wpm_and_four_words():
+    diary_entry = DiaryEntry("My Title", "one two three four")
+    result = diary_entry.reading_time(2)
+    assert result == 2
+
+'''Given a wpm of 2 and text of 3 words, reading_time returns 2 minutes'''
+def test_reading_time_with_two_wpm_and_three_words():
+    diary_entry = DiaryEntry("My Title", "one two three")
+    result = diary_entry.reading_time(2)
+    assert result == 2
+
+'''Given a wpm of 0, reading_time raises an error'''
+def test_reading_time_wpm_of_zero():
+    diary_entry = DiaryEntry("My Title", "one two three")
+    with pytest.raises(Exception) as err:
+        diary_entry.reading_time(0)
+    assert str(err.value) == "Cannot calculate reading time with wpm of 0"
+
+'''Given contents of sxi words, and wpm of 2, and reading_time of 2 minutes, reading_chunk returns first four words'''
+def test_reading_chunk_with_two_wpm_and_two_minutes():
+    diary_entry = DiaryEntry("My Title", "one two three four five six")
+    result = diary_entry.reading_chunk(2, 2)
+    assert result == "one two three four"
+
+'''Given contents of seven words, and wpm of 2 and 1 minute. First time reading_chunk returns on two. Next time, returns three four'''
+def test_reading_chunk_with_two_wpm_and_one_minute_called_multiple_times():
+    diary_entry = DiaryEntry("My title", "one two three four five six seven")
+    assert diary_entry.reading_chunk(2, 1) == "one two"
+    assert diary_entry.reading_chunk(2, 1) == "three four"
+    assert diary_entry.reading_chunk(2, 1) == "five six"
+    assert diary_entry.reading_chunk(1, 1) == "seven"
