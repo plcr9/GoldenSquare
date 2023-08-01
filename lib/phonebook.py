89bc816cb84740016a7eb02efee32bcf716bc87a
plcr9 <paulcooperpc9@gmail.com>
2023-08-01 15:49:22 +0100

    Phonebook new test added

--- lib/phonebook.py
+++ lib/phonebook.py
@@ -1,9 +1,12 @@
+import re
+
 class Phonebook():
     def __init__(self):
         self._numbers = []
 
     def extract_numbers(self, diary_entry):
-        pass
+        numbers = re.findall(r'[0-9]+', diary_entry)
+        self._numbers += numbers
 
     def list_numbers(self):
         return self._numbers
--- tests/test_phonebook.py
+++ tests/test_phonebook.py
@@ -10,3 +10,9 @@ def test_adds_entry_with_no_numbers():
     phone_book = Phonebook()
     phone_book.extract_numbers("My friend's number is not relevant here.")
     assert phone_book.list_numbers() == []
+
+'''When diary entry added with phone number, phone number is reflected in list'''
+def test_extracts_numbers_from_single_entry():
+    phone_book = Phonebook()
+    phone_book.extract_numbers("My friend's number is 07000000000.")
+    assert phone_book.list_numbers() == ['07000000000']
