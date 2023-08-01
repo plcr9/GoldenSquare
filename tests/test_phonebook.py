0a091104d4b11f59958405c4aedf9a7345f425b2
plcr9 <paulcooperpc9@gmail.com>
2023-08-01 16:08:02 +0100

    Phonebook multiple numbers method and test added

--- tests/test_phonebook.py
+++ tests/test_phonebook.py
@@ -16,3 +16,23 @@ def test_extracts_numbers_from_single_entry():
     phone_book = Phonebook()
     phone_book.extract_numbers("My friend's number is 07000000000.")
     assert phone_book.list_numbers() == ['07000000000']
+
+'''When three diary entries with phone numbers added'''
+def test_extracts_numbers_from_multiple_entries():
+    phone_book = Phonebook()
+    phone_book.extract_numbers("Dale's number is 07000000001")
+    phone_book.extract_numbers("Janet's number is 07000000002")
+    phone_book.extract_numbers("Daisy's number is 07000000003")
+    assert phone_book.list_numbers() == ["07000000001", "07000000002", "07000000003"]
+
+'''When an entry with multiple numbers in it is added, all phone numbers are reflected in the list'''
+def test_extracts_multiple_numbers_from_single_entry():
+    phone_book = Phonebook()
+    phone_book.extract_numbers("Dale's number is 07000000001 and Janet's number is 07000000002")
+    assert phone_book.list_numbers() == ["07000000001", "07000000002"]
+
+'''When entry with 11 digit number added that does not start with zero, it is ignored in the numbers list'''
+def test_ignores_numbers_that_do_not_start_with_zero():
+    phone_book = Phonebook()
+    phone_book.extract_numbers("My friend's number is 77000000000")
+    assert phone_book.list_numbers() == []
