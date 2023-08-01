b0717262242158f20a21fda9b3bd1ffd3ff65c1a
plcr9 <paulcooperpc9@gmail.com>
2023-08-01 15:03:58 +0100

    Diary Entry refactor

--- lib/diary_entry.py
+++ lib/diary_entry.py
@@ -18,12 +18,12 @@ class DiaryEntry():
     def reading_time(self, wpm):
         if wpm == 0:
             raise Exception("Cannot calculate reading time with wpm of 0")
-        contents_word_count = len(self._contents.split())
+        contents_word_count = len(self._contents_words())
         return math.ceil(contents_word_count / wpm)
     
     def reading_chunk(self, wpm, minutes):
         words_user_can_read = wpm * minutes
-        words = self._contents.split()
+        words = self._contents_words()
         if self._read_so_far >= len(words):
             self._read_so_far = 0
         chunk_start = self._read_so_far
@@ -31,3 +31,6 @@ class DiaryEntry():
         chunk_words = words[chunk_start:chunk_end]
         self._read_so_far = chunk_end
         return " ".join(chunk_words)
+    
+    def _contents_words(self):
+        return self._contents.split()
